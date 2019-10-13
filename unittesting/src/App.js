55a1ddc1b6b98be1428dea41b40991084fcb74ab
vy3191 <venkyafhs@gmail.com>
2019-10-13 12:50:52 -0400

    Added reset functionality in the app.js and working on display, dashboard components

--- unittesting/src/App.js
+++ unittesting/src/App.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Display from './components/Display';
+import Dashboard from './components/Dashboard';
 import './App.css';
 
+
 function App() {
+  const [balls, setBalls] = useState(0);
+  const [strikes, setStrikes] = useState(0);
+  const [hits, setHits] = useState(0);
+  const [fouls, setFouls] = useState(0);
+
+  const ResetBalls = () => {
+      if(balls >= 4 || strikes >= 3) {
+         setBalls(0);
+         setStrikes(0);
+      } else {
+         setBalls(balls+1);
+      }
+  }
+  const ResetStrikes = () => {
+     if(balls >=4 || strikes >= 3) {
+         setBalls(0);
+         setStrikes(0);
+     } else {
+         setStrikes(strikes+1);
+     }
+  }
+  const ResetHits = () => {
+     setBalls(0);
+     setStrikes(0);
+  }
   return (
     <div className="App">
      <h1>Web Application Testing</h1>
+     <Display />
+     <Dashboard />
     </div>
   );
 }
