e12e57e714caf3659f7aaec9efb3c96e1fc72a2e
vy3191 <venkyafhs@gmail.com>
2019-10-13 13:05:44 -0400

    Added Display and dashboard components with styles-used react-bootstrap

--- unittesting/src/components/Dashboard.js
+++ unittesting/src/components/Dashboard.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import {Button} from 'react-bootstrap';
+
 
 function Dashboard() {
   return (
     <div>
-      
+      <Button variant="primary">Ball</Button>
+      <Button variant="primary">Strike</Button>
+      <Button variant="primary">Hit</Button>
+      <Button variant="primary">Foul</Button> 
     </div>
   )
 }
--- unittesting/src/components/Display.js
+++ unittesting/src/components/Display.js
@@ -3,7 +3,11 @@ import React from 'react'
 function Display() {
   return (
     <div>
-      
+       <div style={{width: '450px', boxShadow: '10px 10px 10px rgba(0,0,0,0.86)', margin: '30px auto', padding: '50px'}}>
+          <h1>Batter Count</h1>
+          <h2>Balls:</h2>
+          <h2>Strikes:</h2>
+       </div>         
     </div>
   )
 }
